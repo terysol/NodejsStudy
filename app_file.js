f7a2e4dfcaa34b0a17113bb6f179e71d315e1b76
Solmin Kim <solminkim03@naver.com>
2021-04-10 12:04:26 +0900

    21.04.10 _ 파일 처리

--- app_file.js
+++ app_file.js
@@ -5,18 +5,26 @@
 // 웹 어플리케이션
 let express=require('express');
 let app=express();
+let bodyParser=require('body-parser');
+let fs=require('fs');
+app.use(bodyParser.urlencoded({extended:false}));
 app.locals.pretty=true;
 app.set('views','./views_file');
 app.set('view engine','pug');
-
-
 
 app.get('/topic/new',function(req,res){
     res.render('new');
 })
 
 app.post('/topic',function(req,res){
-    res.send('HI POST');
+    var title=req.body.title;
+    var description=req.body.description;
+    fs.writeFile('data/' + title,description,(err)=>{
+        if(err){
+            res.status(500).send('Internal Server Error');
+        }
+        res.send('Success !!');
+    });
 })
 
 app.listen(3002, function(){
